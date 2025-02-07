4125b56d7f8ec14a170d659ef7cbfbf5d76d6880
henrylog10 <104786990+henrylog10@users.noreply.github.com>
2025-02-07 14:06:39 -0300

    Add files via upload

---
+++ audino/audino.h
@@ -0,0 +1,26 @@
+/*
+
+  AUDINO.h
+
+  Autor: Eng. Gabriel de Jesus dos Santos Costa
+  Fevereiro de 2025
+
+*/
+
+#ifndef AUDINO_H
+#define AUDINO_H
+
+#include <audino.h>
+
+    void narrador(int indice);
+    void doisd(char valor[]);
+    void tresd(char valor[]);
+    void quatrod(char valor[]);
+    void cincod(char valor[]);
+    void inverter(char dados[20]);
+    void extenso(char valor[]);
+    void sd_wav_player(int speaker_pin, int SD_pin, int qualidade, int volume, int serial_begin);
+
+
+    
+#endif
